379e785cf4da7018983b68ed92680e2b82a48560
ChristinOchoaH <cohoover95@gmail.com>
2019-10-26 23:49:59 -0500

    Acción sobre el botón Calendario

--- app/src/main/java/com/example/asmt_ta2_desafio/MainActivity.java
+++ app/src/main/java/com/example/asmt_ta2_desafio/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.asmt_ta2_desafio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionMenu actionMenu;
+    FloatingActionButton actionButtonC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         actionMenu = (FloatingActionMenu)findViewById(R.id.fabPrincipal);
         actionMenu.setClosedOnTouchOutside(true);
-    }
 
-    public void clicCalendario(View view){
-
+        actionButtonC = (FloatingActionButton)findViewById(R.id.fabCalendario);
+        actionButtonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Calendario.class);
+                startActivity(i);
+            }
+        });
     }
 }
