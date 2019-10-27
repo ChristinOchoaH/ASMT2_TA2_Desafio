1ddea69d48bb185facc810cc3c7723088886e2d7
ChristinOchoaH <cohoover95@gmail.com>
2019-10-26 23:18:58 -0500

    Creación del menú con botón flotante

--- app/src/main/java/com/example/asmt_ta2_desafio/MainActivity.java
+++ app/src/main/java/com/example/asmt_ta2_desafio/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.asmt_ta2_desafio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionMenu actionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionMenu = (FloatingActionMenu)findViewById(R.id.fabPrincipal);
+        actionMenu.setClosedOnTouchOutside(true);
+    }
+
+    public void clicCalendario(View view){
+
     }
 }
