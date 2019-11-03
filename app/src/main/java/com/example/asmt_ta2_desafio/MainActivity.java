e9eb198de751dab1b77fac9e7bc3d8b75da33e60
Danilo Torres Intriago <danilo.ti@hotmail.com>
2019-11-02 20:59:04 -0500

    Parte de Google Maps

--- app/src/main/java/com/example/asmt_ta2_desafio/MainActivity.java
+++ app/src/main/java/com/example/asmt_ta2_desafio/MainActivity.java
@@ -9,11 +9,14 @@ import android.view.View;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionMenu actionMenu;
     FloatingActionButton actionButtonC;
     FloatingActionButton actionButtonV;
+    FloatingActionButton actionButtonM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent a = new Intent(getApplicationContext(), Video.class);
+                startActivity(a);
+            }
+        });
+
+        actionButtonM = (FloatingActionButton)findViewById(R.id.fabMapa);
+        actionButtonM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivity(a);
             }
         });
